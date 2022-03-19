ee640f61fc907f0f7a524124a9762e769e55c6d1
hnqle3 <hoa.nq.le3@gmail.com>
2022-03-19 00:54:38 -0400

    Able to search and display weather information of cities

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -3,23 +3,42 @@ import { connect } from 'react-redux';
 import { Sparklines, SparklinesLine } from "react-sparklines";
 class WeatherList extends Component {
 
+ 
+        renderWeather(cityData){
+            const name = cityData.city.name;
+            return (
+            <tr key={ name }>
+            <td>{ name}</td>
+            <td>{ cityData.list[0].main.temp}</td>
+            <td>{ cityData.list[0].main.pressure}</td>
+            <td>{ cityData.list[0].main.humidity}</td>
+            </tr>
+            )
+            } 
+    
+       
+    
 
- render() {
- return(
- <table className="table table-hover">
- <thead>
- <tr>
- <th>City</th>
- <th>Temperature (Kelvin)</th>
- <th>Pressure (hPa)</th>
- <th>Humidity (%)</th>
- </tr>
- </thead>
- <tbody>
- </tbody>
- </table>
- )
- }
+    render() {
+        return(
+        <table className="table table-hover">
+            <thead>
+                 <tr>
+                    <th>City</th>
+                    <th>Temperature (Kelvin)</th>
+                    <th>Pressure (hPa)</th>
+                    <th>Humidity (%)</th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.props.weather.map(this.renderWeather)}
+            </tbody>
+        </table>
+        )
+        }
+
+
+
 }
 // function mapStateToProps(state){
 // return { weather:state.weather };
